525425b3aaa9283056e6872207657cb9c906f31b
mjmickd <mjmickd@gmail.com>
2023-10-08 18:57:54 -0700

    adding board and starting code

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Constants 
+const COLORS = {
+    '0': 'white',
+    '1': 'LuAnn',
+    '-1': 'Tinsley'
+};
+
+// state variables 
+
+// cached elements 
+
+// event listeners 
+
+//functions 
+
